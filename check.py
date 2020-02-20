efa2919773df68a150660f34c9c6673753f3cd1f
istatmecdl <istatmecdl@gmail.com>
2020-02-20 17:28:48 +0100

    Aggiunte Liste

--- check.py
+++ check.py
@@ -5,7 +5,7 @@ Modified on 20/01/2020
 @author: Francesco Pugliese
 '''
 
-import tensorflow as tf
+#import tensorflow as tf
 import keras as tf
 import pandas as pd
 import numpy as np
@@ -13,3 +13,9 @@ import sklearn as sc
 import matplotlib as mb
 import pdb
 print("Hello world!")
+
+lista = [123, "spam", 1.23, "eggs"]
+lista1 = lista + ["Francesco"]
+print(lista, lista[0], lista[0:2])
+print(lista1)
+print(lista * 2)
