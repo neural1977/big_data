96a741de382cd7e4a9e158ca97a2d8bc0fcad459
istatmecdl <istatmecdl@gmail.com>
2020-02-20 15:07:40 +0100

    Ho modificato check.py

---
+++ check.py
@@ -0,0 +1,15 @@
+'''
+Created on 20/02/2020
+Modified on 20/01/2020
+
+@author: Francesco Pugliese
+'''
+
+import tensorflow as tf
+import keras as tf
+import pandas as pd
+import numpy as np
+import sklearn as sc
+import matplotlib as mb
+import pdb
+print("Hello world!")
